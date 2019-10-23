177af293e79f2a7ae740ad974fcfd2430935f38e
Redion <rkadilliu@gmail.com>
2019-10-23 16:24:07 -0400

    tried extra credit

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,25 +1,45 @@
 "use strict";
 
 // Complete this algo
-const binarySearch = (array, target) => {
-  if (array.length === 1) {
-    if (target === array[0]) {
-      return true;
-    } else {
-      return false;
-    }
+const binarySearch = (
+  array,
+  target,
+  start = 0,
+  end = array.length - 1,
+  direction = "left"
+) => {
+  // if (array.length === 1) {
+  //   if (target === array[0]) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  if (end - start === 1) {
+    return false;
   }
-  let midpointIndex = Math.floor(array.length / 2);
+  let midpointIndex;
+  if (direction === "left") {
+    midpointIndex = Math.floor((start + end) / 2);
+  } else {
+    midpointIndex = Math.ceil((start + end) / 2);
+  }
+
   let midpointValue = array[midpointIndex];
 
   if (midpointValue === target) {
     return true;
-  } else if (target < midpointValue) {
-    let firstHalf = array.slice(0, midpointIndex);
-    return binarySearch(firstHalf, target);
+  }
+
+  if (target < midpointValue) {
+    // let firstHalf = array.slice(0, midpointIndex);
+    direction = "left";
+    return binarySearch(array, target, start, midpointIndex);
   } else {
-    let secondHalf = array.slice(midpointIndex, array.length);
-    return binarySearch(secondHalf, target);
+    // let secondHalf = array.slice(midpointIndex, array.length);
+    direction = "right";
+    return binarySearch(array, target, midpointIndex, end);
   }
 };
 
